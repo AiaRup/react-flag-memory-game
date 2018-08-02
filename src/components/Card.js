92511d6f2107dd7840f5595994b21b831600c0be
meir2017 <66meir46@gmail.com>
2018-08-02 18:04:09 +0300

    add style01

--- src/components/Card.js
+++ src/components/Card.js
@@ -30,7 +30,7 @@ class Card extends Component {
   render() {
     return (
 
-      <div className="col-12 col-sm-6 col-md-3">
+      <div className="col-12 col-sm-6 col-md-3" >
         <div className="Card " onClick={this.cardClicked}>
           <img className="ImgCard" src={this.props.isMatch ? `https://www.countryflags.io/${this.props.code}/shiny/64.png` : "card1.jpg"} alt="" />
           {this.state.isShowing && !this.state.isCorrect && <FlagTrivia name={this.props.name} onUserAnswer={this.onUserAnswer} code={this.props.code} />}
